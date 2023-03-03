70716d4e52d75c525f408bfc1c79fc6b0c93fbfb
Ryna <100812735+CapnRyna@users.noreply.github.com>
2023-03-02 20:58:04 -0500

    Add files via upload

---
+++ type.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ["Frontend Developer", "Part-time comedian", "Nature Activist", "Dog Lover"],
+    typeSpeed: 150,
+    backspeed: 50,
+    backDelay: 1000,
+    loop: true
+
+})
